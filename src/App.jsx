c2df99be4bc087c47fe258855a114cce1e4cbc4a
Mukesh Verma <mars.mukesh02@gmai.com>
2023-09-20 12:17:23 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import HeroSection from "./component/Hero";
+import Navigation from "./component/navigation"; "./"
+const App = () =>{
+    return(<div>
+        <Navigation />
+        <HeroSection />
+    </div>
+    ) 
+  };
+  
+  export default App;
+
